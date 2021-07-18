d8fd33bb5a8abe1fefd3a7476197a59277242607
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-19 00:44:56 +0300

    finished the paddle logic

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,7 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
 
 public class Paddle extends Rectangle {
 
@@ -14,13 +12,58 @@ public class Paddle extends Rectangle {
     this.id = id;
   }
 
-  public void keyPressed(KeyEvent e) {}
+  public void keyPressed(KeyEvent e) {
+    switch (id) {
+      case 1:
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+          setYDirection(-speed);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+          setYDirection(speed);
+        }
+        break;
+      case 2:
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+          setYDirection(-speed);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+          setYDirection(speed);
+        }
+        break;
+    }
+  }
 
-  public void keyReleased(KeyEvent e) {}
+  public void keyReleased(KeyEvent e) {
+    switch (id) {
+      case 1:
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+          setYDirection(0);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+          setYDirection(0);
+        }
+        break;
+      case 2:
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+          setYDirection(0);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+          setYDirection(0);
+        }
+        break;
+    }
+  }
 
-  public void setYDirection(int ydirection) {}
+  public void setYDirection(int yDirection) {
+    yVelocity = yDirection;
+  }
 
-  public void move() {}
+  public void move() {
+    y = y + yVelocity;
+  }
 
-  public void draw(Graphics g) {}
+  public void draw(Graphics g) {
+    if (id == 1) g.setColor(Color.blue); else g.setColor(Color.red);
+    g.fillRect(x, y, width, height);
+  }
 }
